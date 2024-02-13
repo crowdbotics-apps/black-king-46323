0738f8a5ad754fed956289d2be749d6900323378
Crowdbotics <team@crowdbotics.com>
2024-02-13 20:01:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-king-46323",
+  url: "https://black-king-46323.botics.co",
+  api: "https://black-king-46323.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
